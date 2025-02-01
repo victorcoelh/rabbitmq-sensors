39321dd37056d4f2404f2926cdcf33688bfe7dd2
Victor <victorcoelho250602@gmail.com>
2025-02-01 15:49:43 -0300

    Added unit to publisher message

--- src/bin/publisher.rs
+++ src/bin/publisher.rs
@@ -10,26 +10,34 @@ pub async fn main() {
 
     let sensor_name = args.get(1).expect("Please insert a sensor name.").clone();
     let sensor_type = args.get(2).expect("Missing a sensor type.").clone();
+    let unit = args.get(3).expect("Missing unit").parse().unwrap();
     let min_value = args
-        .get(3)
+        .get(4)
         .expect("Missing minimum value.")
         .parse()
         .expect("Minimum value should be a numerical value.");
     let max_value = args
-        .get(4)
+        .get(5)
         .expect("Missing maximum value.")
         .parse()
         .expect("Maximum value should be a numerical value.");
 
     let handle = tokio::task::spawn(async move {
-        create_iot_sensor(sensor_type, sensor_name, min_value, max_value).await
+        create_iot_sensor(sensor_type, sensor_name, unit, min_value, max_value).await
     });
 
     tokio::join!(handle).0.unwrap();
 }
 
-async fn create_iot_sensor(sensor_type: String, sensor_name: String, min: f32, max: f32) {
+async fn create_iot_sensor(
+    sensor_type: String,
+    sensor_name: String,
+    unit: String,
+    min: f32,
+    max: f32,
+) {
     let broker = BrokerConnection::connect(false, &sensor_type, &sensor_name).await;
+    println!("Sensor started!");
 
     broker
         .send_message(&format!("{sensor_type}.{sensor_name}: online"))
@@ -39,7 +47,9 @@ async fn create_iot_sensor(sensor_type: String, sensor_name: String, min: f32, m
         let sensor_reading = generate_sensor_reading(min, max, 0.2);
         if sensor_reading < min || sensor_reading > max {
             broker
-                .send_message(&format!("{sensor_type}.{sensor_name}: {sensor_reading:.2}"))
+                .send_message(&format!(
+                    "{sensor_type}.{sensor_name}: {sensor_reading:.2}{unit}"
+                ))
                 .await;
         }
 
